8a7f8e80443341efb3b6290a7a256c3fd0aabfb9
Abhishek Dwivedi <AI-BLUE-BOOK>
2023-06-18 09:15:24 +0530

    first commit dated 18/6/23

---
+++ Near_by/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NearByConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Near_by'
--- PeerConnect/settings.py
+++ PeerConnect/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'PAGES.apps.PagesConfig',
+    'Near_by.apps.NearByConfig',
 ]
 
 MIDDLEWARE = [
